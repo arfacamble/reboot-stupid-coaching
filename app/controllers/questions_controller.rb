1b12ca96624cd212ad7df0c5ec51784de2adcb07
Arthur Campbell <arfa.camble@gmail.com>
2021-05-21 16:34:13 +0100

    so

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,19 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    puts params
+    @question = params[:question]
+    @answer = coach_responds_to @question
+  end
+
+  private
+
+  def coach_responds_to(question)
+    if question == 'I am going to work'
+      'Great!'
+    elsif question[-1] == '?'
+      'Silly question, get dressed and go to work!'
+    else
+      'I don\'t care, get dressed and go to work!'
+    end
   end
 end
